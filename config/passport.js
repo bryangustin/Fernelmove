ab85e38873f921479a67838fef780daa718eebe0
Sylvain Durant <sy.durant@hotmail.com>
2021-02-03 15:15:53 +0100

    added reset password feature

--- config/passport.js
+++ config/passport.js
@@ -4,12 +4,28 @@ const User = require("../models/user");
 
 module.exports = function(passport) {
     passport.use(
-        new LocalStrategy({usernameField : 'email'},(email,password,done)=> {
+        new LocalStrategy({usernameField : 'usernameEmail'},(usernameEmail,password,done)=> {
             //match user
-            User.findOne({email : email})
+            User.findOne({email : usernameEmail})
             .then((user)=>{
                 if(!user) {
-                    return done(null,false,{message : 'Aucun compte ne correspond à cet email'});
+                    User.findOne({name : usernameEmail})
+                    .then((user)=>{
+                        if(!user) {
+                            return done(null,false,{message : 'Aucun compte ne correspond à cet email ou ce pseudo'});
+                        }
+                        //match pass
+                        bcrypt.compare(password,user.password,(err,isMatch)=>{
+                            if(err) throw err;
+        
+                            if(isMatch) {
+                                return done(null,user);
+                            } else {
+                                return done(null,false,{message : 'Mot de passe incorrect'});
+                            }
+                        })
+                    })
+                    .catch((err)=> {console.log(err)})
                 }
                 //match pass
                 bcrypt.compare(password,user.password,(err,isMatch)=>{
