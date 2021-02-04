15d7f5ff7b38379d66458c05bf0eaa517a863947
Sylvain Durant <sy.durant@hotmail.com>
2021-02-04 15:05:42 +0100

    working on maps

--- public/js/map.js
+++ public/js/map.js
@@ -11,42 +11,69 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition((position) => {
         me.push(position.coords.longitude);
         me.push(position.coords.latitude);
+        me = [4.9792, 50.5438]; // TO DELETE FOR REAL LOCALIZATION
+
+        //Printing map
+
+        mapboxgl.accessToken = 'pk.eyJ1Ijoib2Rhc3lsdmFpbiIsImEiOiJja2txbG1kaWwwNTNmMm9wY3hzbWpoZm94In0.KqoXKfntalmfcnKaaOgxGw';
+
+        let geojson = {
+          me: {
+            geometry: {
+              coordinates: me
+            },
+            properties: {
+              title: 'You are here.',
+              description: 'none'
+            }
+          }
+        };
 
         fetch(url+convivialite)
         .then(response => response.json())
-        .then(data => {
-            me = [4.9792, 50.5438]; // TO DELETE FOR REAL LOCALIZATION
-            //Printing map
-            mapboxgl.accessToken = 'pk.eyJ1Ijoib2Rhc3lsdmFpbiIsImEiOiJja2txbG1kaWwwNTNmMm9wY3hzbWpoZm94In0.KqoXKfntalmfcnKaaOgxGw';
+        .then(dataConvivialite => {
 
-            let map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-                center: me, // starting position [lng, lat] 
-                zoom: 11 // starting zoom
-            });
+          data.records.forEach(elem => {
+              let point = {};
+              point.pos = [];
+              point.pos.push(elem.fields.point_geocodage[1]);
+              point.pos.push(elem.fields.point_geocodage[0]);
+              point.text = elem.fields.remarques;
 
-            let points = [{pos:me, text: "You are here."}];
+              points.push(point);
+          });
 
-            data.records.forEach(elem => {
-                let point = {};
-                point.pos = [];
-                point.pos.push(elem.fields.point_geocodage[1]);
-                point.pos.push(elem.fields.point_geocodage[0]);
-                point.text = elem.fields.remarques;
+          fetch(url+ecoles)
+          .then(response => response.json())
+          .then(dataEcoles => {
 
-                points.push(point);
-            });
+            fetch(url+livres)
+            .then(response => response.json())
+            .then(dataLivres => {
+        
+              var map = new mapboxgl.Map({
+                  container: 'map',
+                  style: 'mapbox://styles/mapbox/streets-v11',
+                  center: me,
+                  zoom: 11
+              });
+          
+              // add markers to map
+              geojson.features.forEach(function(marker) {
 
-            points.forEach((point) => {
-                let popup = new mapboxgl.Popup({ offset: 25 }).setText(
-                point.text
-                );
-                let marker = new mapboxgl.Marker()
-                .setLngLat(point.pos)
-                .setPopup(popup)
+                // create a HTML element for each feature
+                var el = document.createElement('div');
+                el.className = 'markerRed';
+
+                // make a marker for each feature and add to the map
+                new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
                 .addTo(map);
+              });
             })
+          })
         })        
     });
   } else {
@@ -55,3 +82,58 @@ function getLocation() {
 }
 
 getLocation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // mapboxgl.accessToken = 'pk.eyJ1Ijoib2Rhc3lsdmFpbiIsImEiOiJja2txbG1kaWwwNTNmMm9wY3hzbWpoZm94In0.KqoXKfntalmfcnKaaOgxGw';
+
+            // let map = new mapboxgl.Map({
+            //     container: 'map',
+            //     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+            //     center: me, // starting position [lng, lat] 
+            //     zoom: 11 // starting zoom
+            // });
+
+            // let points = [{pos:me, text: "You are here."}];
+
+            // data.records.forEach(elem => {
+            //     let point = {};
+            //     point.pos = [];
+            //     point.pos.push(elem.fields.point_geocodage[1]);
+            //     point.pos.push(elem.fields.point_geocodage[0]);
+            //     point.text = elem.fields.remarques;
+
+            //     points.push(point);
+            // });
+
+            // points.forEach((point) => {
+            //     let popup = new mapboxgl.Popup({ offset: 25 }).setText(
+            //     point.text
+            //     );
+            //     let marker = new mapboxgl.Marker()
+            //     .setLngLat(point.pos)
+            //     .setPopup(popup)
+            //     .addTo(map);
+            // })
