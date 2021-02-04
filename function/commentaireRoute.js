f19e43183b2bd6428d521b47f82b5153d898291a
Makraiyassin <makrai.yassin@gmail.com>
2021-02-04 10:41:31 +0100

    nodemailer

--- function/commentaireRoute.js
+++ function/commentaireRoute.js
@@ -1,14 +1,41 @@
-//test commentaire:
+//Commentaire:
 const Commentaire = require("../models/commentaires");
+
+//MAIL
+const nodemailer = require("nodemailer");
 
 const commentaireRoute = function (app) {
 
     app.post('/commentaire',(req,res)=>{
         console.log(req.body);
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL,
+                pass: process.env.PASS
+            }
+        });    
+        
+        transporter.sendMail({
+            from: process.env.MAIL,
+            to: "makrai.yassin@gmail.com",
+            subject: `Message de ${"req.body.pseudo"}`,
+            text: req.body.commentaire,
+        }, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    
         let commentaire = new Commentaire({
             commentaire : req.body.commentaire
         })
+
         commentaire.save()
+
         res.redirect('back');
     })
 }
