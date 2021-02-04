6d4deefe105637efdcaf6d81501bef5b09a6f176
Sylvain Durant <sy.durant@hotmail.com>
2021-02-04 16:33:01 +0100

    commit before pull

--- public/js/map.js
+++ public/js/map.js
@@ -17,7 +17,17 @@ function getLocation() {
 
         mapboxgl.accessToken = 'pk.eyJ1Ijoib2Rhc3lsdmFpbiIsImEiOiJja2txbG1kaWwwNTNmMm9wY3hzbWpoZm94In0.KqoXKfntalmfcnKaaOgxGw';
 
-        let geojson = {
+        class Point {
+          constructor(lng, lat, title, description) {
+            this.geometry = {coordinates:[lng, lat]};
+            this.properties = {
+              title: title,
+              description: description
+            }
+          }
+        }
+
+        let geojson = {  
           me: {
             geometry: {
               coordinates: me
@@ -33,37 +43,69 @@ function getLocation() {
         .then(response => response.json())
         .then(dataConvivialite => {
 
-          data.records.forEach(elem => {
-              let point = {};
-              point.pos = [];
-              point.pos.push(elem.fields.point_geocodage[1]);
-              point.pos.push(elem.fields.point_geocodage[0]);
-              point.text = elem.fields.remarques;
+          let convivialites = [];
+          dataConvivialite.records.forEach(elem => {
 
-              points.push(point);
+            convivialites.push(new Point(
+              elem.fields.point_geocodage[1], 
+              elem.fields.point_geocodage[0], 
+              elem.fields.localisation,
+              elem.fields.remarques
+            ));
+
           });
+
+          geojson.convivialites = convivialites;
 
           fetch(url+ecoles)
           .then(response => response.json())
           .then(dataEcoles => {
 
+            let ecoles = [];
+            dataEcoles.records.forEach(elem => {
+  
+              ecoles.push(new Point(
+                elem.fields.point_geocodage[1], 
+                elem.fields.point_geocodage[0], 
+                elem.fields.nom,
+                elem.fields.localisation +', '+elem.fields.village
+              ));
+  
+            });
+  
+            geojson.ecoles = ecoles;
+
             fetch(url+livres)
             .then(response => response.json())
             .then(dataLivres => {
         
-              var map = new mapboxgl.Map({
+              let livres = [];
+              dataLivres.records.forEach(elem => {
+    
+                livres.push(new Point(
+                  elem.fields.point_geocodage[1], 
+                  elem.fields.point_geocodage[0], 
+                  elem.fields.localisation,
+                  elem.fields.remarques
+                ));
+    
+              });
+    
+              geojson.livres = livres;
+
+              let map = new mapboxgl.Map({
                   container: 'map',
                   style: 'mapbox://styles/mapbox/streets-v11',
                   center: me,
                   zoom: 11
               });
           
-              // add markers to map
-              geojson.features.forEach(function(marker) {
+              // add convivialites markers to map
+              geojson.convivialites.forEach(function(marker) {
 
                 // create a HTML element for each feature
-                var el = document.createElement('div');
-                el.className = 'markerRed';
+                let el = document.createElement('div');
+                el.className = 'markerPurple';
 
                 // make a marker for each feature and add to the map
                 new mapboxgl.Marker(el)
@@ -72,10 +114,55 @@ function getLocation() {
                 .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
                 .addTo(map);
               });
+
+              // add ecoles markers to map
+              geojson.ecoles.forEach(function(marker) {
+
+                // create a HTML element for each feature
+                let el = document.createElement('div');
+                el.className = 'markerGreen';
+
+                // make a marker for each feature and add to the map
+                new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+              });
+
+              // add livres markers to map
+              geojson.livres.forEach(function(marker) {
+
+                // create a HTML element for each feature
+                let el = document.createElement('div');
+                el.className = 'markerBlue';
+
+                // make a marker for each feature and add to the map
+                new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+              });             
+
+              // add user position markers to map
+
+              // create a HTML element for the user
+              let el = document.createElement('div');
+              el.className = 'markerRed';
+
+              // make a marker for the user and add to the map
+              new mapboxgl.Marker(el)
+              .setLngLat(geojson.me.geometry.coordinates)
+              .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML('<h3>' + geojson.me.properties.title + '</h3>'))
+              .addTo(map);   
+              
             })
           })
         })        
-    });
+      }
+    );
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
